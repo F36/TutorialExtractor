88efcd131f0570fef75cdf92eaba2b577e5b54a5
GOSHROW <abhroindia@gmail.com>
2020-06-28 03:20:54 +0530

    Multiple Page Accumulation

--- main.py
+++ main.py
@@ -7,25 +7,48 @@ class TPExtractor:
     def __init__(self):
         self.start = '<!-- Tutorial Content Starts Here -->'
         self.end = '<!-- Tutorial Content Ends Here -->'
-        self.sep = 'hr{ border: 1px dashed #000; width: 50%; margin: auto; margin-top: 5%; margin-bottom: 5%; }>'
-    def TutorialsPoint(self, URL, iterations = 1, filename = 'out'):
+        self.sep = 'hr{ border: 1px dashed #000; width: 50%; margin: auto; margin-top: 5%; margin-bottom: 5%; }'
+        self.domain = 'https://www.tutorialspoint.com/'
+
+    def getPDF(self, filename = 'out'):
+        return
+
+    def getNext(self, content):
+        try:
+            soup = BeautifulSoup(content, 'html.parser')
+            outerdiv = soup.find_all("div", attrs = {"id":"bottom_navigation"})
+            innerdiv = [iter.find('div', attrs = {"class" : "nxt-btn"} ) for iter in outerdiv]
+            nextURL = [iter.find('a')["href"] for iter in innerdiv]
+            return self.domain + nextURL[-1]
+        except Exception as E:
+            print(E)
+            return False
+
+    def addToHTML(self, URL, iterations = 1, filename = 'out'):
         if iterations < 1:
-            return
+            return self.getPDF(filename)
+            
         req = request.urlopen(URL)
         html = req.read().decode('utf-8')
         content = html.split(self.start)[1].split(self.end)[0]
-        filename += '.html'
-        if os.path.exists(filename):
+        htmlFilename = filename + '.html'
+        if os.path.exists(htmlFilename):
             fileOption = 'a'
         else:
             fileOption = 'w'
             content = self.sep + content
         
-        f = open(filename, fileOption)
-        f.write(content + '<hr>')
+        f = open(htmlFilename, fileOption)
+        f.write(content + '<hr><hr>')
         f.close()
+        
+        nextURL = self.getNext(content)
+        if not nextURL:
+            self.addToHTML(URL, iterations = 0, filename = filename)
+        else:
+            self.addToHTML(nextURL, iterations = iterations - 1, filename = filename)
 
 #  TEST
-# if __name__ == '__main__':
-#     ob = TPExtractor()
-#     ob.TutorialsPoint('https://www.tutorialspoint.com/data_structures_algorithms/index.htm')
+if __name__ == '__main__':
+    ob = TPExtractor()
+    ob.addToHTML('https://www.tutorialspoint.com/data_structures_algorithms/index.htm', 3)
