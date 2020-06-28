6b6ea42242fc53f6a929eaddb8a1486389c62655
GOSHROW <abhroindia@gmail.com>
2020-06-29 02:54:13 +0530

    refactored

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from urllib import request
 from bs4 import BeautifulSoup
 import json
 import os
+import sys
+import pdfkit
 
 class TPExtractor:
-    def __init__(self):
+
+    def __init__(self, argsList):
         self.start = '<!-- Tutorial Content Starts Here -->'
         self.end = '<!-- Tutorial Content Ends Here -->'
-        self.sep = 'hr{ border: 1px dashed #000; width: 50%; margin: auto; margin-top: 5%; margin-bottom: 5%; }'
         self.domain = 'https://www.tutorialspoint.com/'
-
+        url = str(argsList[1])
+        iters = int(argsList[2]) if len(argsList) > 2 else 1
+        outFile = str(argsList[3]) if len(argsList) > 3 else 'out'
+        for i in ['.html', '.pdf']:
+            if os.path.exists(outFile + i):
+                os.remove(outFile + i)
+        self.addToHTML(url, iters, outFile)
+    
     def getPDF(self, filename = 'out'):
-        return
+        try:
+            pdfkit.from_file(filename + '.html', filename + '.pdf')
+        except Exception as e:
+            print("Empty File Possible" + e)
 
     def getNext(self, content):
         try:
@@ -24,19 +36,22 @@ class TPExtractor:
             print(E)
             return False
 
+    def absoluteHREFs(self, content):
+        return content
+
     def addToHTML(self, URL, iterations = 1, filename = 'out'):
+        print(str(iterations) + " pages to go . . .")
         if iterations < 1:
             return self.getPDF(filename)
-            
         req = request.urlopen(URL)
         html = req.read().decode('utf-8')
         content = html.split(self.start)[1].split(self.end)[0]
+        content = self.absoluteHREFs(content)
         htmlFilename = filename + '.html'
         if os.path.exists(htmlFilename):
             fileOption = 'a'
         else:
             fileOption = 'w'
-            content = self.sep + content
         
         f = open(htmlFilename, fileOption)
         f.write(content + '<hr><hr>')
@@ -50,5 +65,5 @@ class TPExtractor:
 
 #  TEST
 if __name__ == '__main__':
-    ob = TPExtractor()
-    ob.addToHTML('https://www.tutorialspoint.com/data_structures_algorithms/index.htm', 3)
+    argsList = sys.argv
+    TPExtractor(argsList)
