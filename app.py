7516f882bb5709d18414696dbe3c75b79a167c75
GOSHROW <abhroindia@gmail.com>
2020-06-29 19:32:46 +0530

    POST and form input created

---
+++ app.py
@@ -0,0 +1,18 @@
+from main import TPExtractor
+from flask import Flask, request, render_template
+
+app = Flask(__name__) 
+
+@app.route('/')
+def input():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def input_post():
+    URL = request.form['URL']
+    pages = request.form['pages']
+    outFile = request.form['outFile']
+    TPExtractor(["dummy", URL, pages, outFile])
+
+if __name__ == '__main__':
+    app.run()
