e01d4ecdcbb5c5b328b16003b343858afcb344e0
GOSHROW <abhroindia@gmail.com>
2020-06-30 19:08:51 +0530

    Downloadable Link created

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from main import TPExtractor
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import time
 
 app = Flask(__name__) 
@@ -14,8 +14,15 @@ def input_post():
     pages = request.form['pages']
     outFile = request.form['outFile']
     TPExtractor(["dummy", URL, pages, outFile])
-    message = '<br><br>Your File is Ready to <a href="' + outFile + '.pdf"> Download </a>'
+    message = '<br><br>Your File is Ready to <a href="./downloads/' + outFile + '.pdf"> Download </a>'
     return render_template("index.html") + message
+
+@app.route('/downloads/<outFile>')
+def getDownload(outFile) :
+    try:
+        return send_file(filename_or_fp = str(outFile), as_attachment=True)
+    except Exception as E:
+        print(E)
 
 if __name__ == '__main__':
     app.run()
