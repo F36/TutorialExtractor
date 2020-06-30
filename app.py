f3ad4e64099b3a5d3266da0e7a25274d47b6d1b6
GOSHROW <abhroindia@gmail.com>
2020-07-01 02:14:16 +0530

    file not found

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def input_post():
     pages = request.form['pages']
     outFile = request.form['outFile']
     TPExtractor(["dummy", URL, pages, outFile])
-    message = '<br><br>Your File is Ready to <a href="./downloads/' + outFile + '.pdf"> Download </a>'
+    message = '<br><br>Your File is Ready to <a href="./' + outFile + '"> Download </a>'
     return render_template("index.html") + message
 
-@app.route('/downloads/<outFile>')
+@app.route('/<outFile>')
 def getDownload(outFile) :
     try:
-        return send_file(filename_or_fp = str(outFile), as_attachment=True)
+        return send_file(filename_or_fp = str(outFile + '.pdf'), as_attachment=True)
     except Exception as E:
         print(E)
 
